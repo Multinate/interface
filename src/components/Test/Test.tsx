e936fb5023313a51fbb0b8ebd0f9d3f7c3920aa9
Eric Mong <m.changhsi@gmail.com>
2023-03-16 14:51:13 +0800

    initial commit

---
+++ src/components/Test/Test.tsx
@@ -0,0 +1,9 @@
+import { TestContainer, TestTitle } from "./TestStyle";
+
+const Test = () => {
+  return <TestContainer>
+    <TestTitle>This is a test</TestTitle>
+  </TestContainer>
+}
+
+export default Test;
---
+++ src/components/Test/TestStyle.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const TestContainer = styled.div`
+  position: relative;
+`
+
+export const TestTitle = styled.h1`
+  color: red;
+`
