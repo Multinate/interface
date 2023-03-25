3d001e72157390276c6b1d54dcba4c6d21de457c
Eric Mong <m.changhsi@gmail.com>
2023-03-25 15:53:04 +0800

    Init config files

---
+++ src/utils/Config.ts
@@ -0,0 +1,7 @@
+const config = {
+  optimism: process.env.NEXT_PUBLIC_OPTIMISMGOERLI || '',
+  goerli: process.env.NEXT_PUBLIC_GOERLI || '',
+  mumbai: process.env.NEXT_PUBLIC_MUMBAI || '',
+}
+
+export default config
