689450e260b0c875f5e80ddd24061d819dd4bfb3
Eric Mong <m.changhsi@gmail.com>
2023-03-26 02:27:23 +0800

    Add snapshot subgraph

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -3,6 +3,7 @@ const config = {
   goerli: process.env.NEXT_PUBLIC_GOERLI || '',
   mumbai: process.env.NEXT_PUBLIC_MUMBAI || '',
   snapshot_spacename: process.env.NEXT_PUBLIC_SNAPSHOT_SPACE_NAME || '',
+  snapshot_graphql: process.env.NEXT_PUBLIC_SNAPSHOT_GRAPHQL || '',
 }
 
 export default config
