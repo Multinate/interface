a41a9f1d2a673470ae79886bd87c8720d6f0c10f
Eric Mong <m.changhsi@gmail.com>
2023-03-25 23:07:37 +0800

    Update config

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -2,6 +2,7 @@ const config = {
   optimism: process.env.NEXT_PUBLIC_OPTIMISMGOERLI || '',
   goerli: process.env.NEXT_PUBLIC_GOERLI || '',
   mumbai: process.env.NEXT_PUBLIC_MUMBAI || '',
+  snapshot_spacename: process.env.NEXT_PUBLIC_SNAPSHOT_SPACE_NAME || '',
 }
 
 export default config
