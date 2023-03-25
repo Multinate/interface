11d5d4e9f6869d0f484a34e863c0f62a6cc571f0
Eric Mong <m.changhsi@gmail.com>
2023-03-25 23:06:06 +0800

    Update textfield ui component for textarea

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,19 +1,36 @@
 import { FC, useState } from 'react'
-import { TextFieldContainer, TextFieldInput, TextFieldLabel } from './style'
+import { TextFieldAreaContainer, TextFieldAreaInput, TextFieldContainer, TextFieldInput, TextFieldLabel } from './style'
 
 interface IProps {
   label: string
   type: string
+  handleChange: (text: string) => void
 }
 
-const TextField: FC<IProps> = ({ label, type }) => {
+const TextField: FC<IProps> = ({ label, type, handleChange }) => {
   const [text, setText] = useState<string>('')
   const [focus, setFocus] = useState<boolean>(false)
 
   const handleChangeText = (e: string) => {
+    handleChange(e)
     setText(e)
   }
 
+  if (type === 'textarea') {
+    return (
+      <TextFieldAreaContainer>
+        <TextFieldAreaInput
+          value={text}
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+          onChange={(e) => handleChangeText(e.target.value)}
+        ></TextFieldAreaInput>
+        <TextFieldLabel isText={text.length > 0} isFocus={focus}>
+          {label}
+        </TextFieldLabel>
+      </TextFieldAreaContainer>
+    )
+  }
   return (
     <TextFieldContainer>
       <TextFieldInput
