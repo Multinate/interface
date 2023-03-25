a528cd2e54a94b9b54fb529044f1edc4083b8dfb
Eric Mong <m.changhsi@gmail.com>
2023-03-25 16:53:24 +0800

    Reduce chevron width

--- src/components/DropdownField/index.tsx
+++ src/components/DropdownField/index.tsx
@@ -29,7 +29,7 @@ const DropdownField: FC<IProps> = ({ label, options, activeSelection, handleSele
           <p>{activeSelection.label}</p>
           <span>
             <svg width="17" height="10" viewBox="0 0 17 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1 1L8.5 8.5L16 1" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M1 1L8.5 8.5L16 1" stroke="black" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
           </span>
         </DropdownFieldInput>
